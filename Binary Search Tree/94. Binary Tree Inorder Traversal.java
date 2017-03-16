cfbe9d66d9ecbf76611921217beedfc556a710e8
optoelectronicDog <lindui1994@gmail.com>
2017-03-16 00:58:01 -0700

    Needed to add other non-recursive solutions!

---
+++ Binary Search Tree/94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,40 @@
+/**
+ * Solution1: Recursive 
+ * 
+ * Notes: 1. use helper! And remember to take in both root and list, because both needed to be updated.
+ *        2. inorder : left -> root -> right
+ * 
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>(); // List is abstract and cannot be instantiate
+        }
+        inorder(root, list);
+        return list;
+    }
+    private ArrayList<Integer> list = new ArrayList<Integer>();
+    private void inorder(TreeNode root, ArrayList<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+        
+    }
+    
+}
+
+**************************************************************
