7434156780cf12df576033f1c2107ea82bd0fe81
optoelectronicDog <lindui1994@gmail.com>
2017-03-15 19:32:05 -0700

    Create 83. Remove Duplicates from Sorted List.java

---
+++ LinkedList/83. Remove Duplicates from Sorted List.java
@@ -0,0 +1,61 @@
+/**
+ * Thoughts: 1. the linkedlist is sorted, so there is no such [1,2,2,3,4,2]
+ *           2. be very careful to the nullpointer exception, 
+ *              check every condition to make sure won't transpass the boundary
+ * 
+ * Time Complexity: O(N^2), too slow 
+ * 
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode runner = head;
+        while (runner != null && runner.next != null) {
+   /* in case of more than 2 consecutive duplicate elements, need another loop rather than a single if statement! */
+            while (runner.next != null && (runner.next.val == runner.val)) { 
+                runner.next = runner.next.next;
+            }
+            runner = runner.next; // increment
+        }
+        return head;
+    }
+}
+
+
+**************************************************************************************
+// Another solution with time complexity O(N)
+
+/**
+ * Time Complexity: O(N)
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode runner = head;
+        while (runner != null && runner.next != null) {
+            if (runner.val == runner.next.val) {
+                runner.next = runner.next.next;
+            } else {
+                runner = runner.next;
+            }
+        }
+        return head;
+    }
+}
