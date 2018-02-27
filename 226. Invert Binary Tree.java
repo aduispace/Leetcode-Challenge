ce3bb05f34f712d90479a6e3ab07c96e96c83913
DLCoder <lindui1994@gmail.com>
2018-02-26 21:57:14 -0800

    Create 226. Invert Binary Tree.java

---
+++ 226. Invert Binary Tree.java
@@ -0,0 +1,35 @@
+Invert a binary tree.
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+to
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        // 根不用invert，所以先分，再治
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.right = left;
+        root.left = right;
+        
+        return root;
+    }
+}
